bb884fd2855167a05d7de054f1e2accdbfbd6dbc
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2021-03-06 23:20:48 +0100

    Update tests

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders upload your image h1 text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = screen.getByText(/Upload your Image/i);
+  expect(h1Element).toBeInTheDocument();
 });
