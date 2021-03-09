08aef74eaa7f0d44f0a2a52d935dc6c8eb713219
Minh-Quan Nguyen <minhquan.nguyen.etu@univ-lille.fr>
2021-03-09 09:57:37 +0100

    Delete tests

--- src/components/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders upload your image h1 text', () => {
-  render(<App />);
-  const h1Element = screen.getByText(/Upload your Image/i);
-  expect(h1Element).toBeInTheDocument();
-});
